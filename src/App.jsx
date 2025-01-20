1c9ac2e03eb978251fbd77b25440682e25afeade
arbazkhan97 <arbazkhan97432@gmail.com>
2025-01-20 14:40:19 +0530

    add media card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 import './App.css'
 import Submit from './Submit'
+import Media from './Media'
 
 function App() {
   
 
   return (
     <>
-    
     <Submit/>
+    {/* <Media/> */}
     </>
     
       
