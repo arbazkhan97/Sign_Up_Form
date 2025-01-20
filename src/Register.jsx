16688602d28626b45b5fbeccdb07af994ffa0d40
arbazkhan97 <arbazkhan97432@gmail.com>
2025-01-20 13:40:28 +0530

    add curley brackets

--- src/Register.jsx
+++ src/Register.jsx
@@ -3,7 +3,7 @@
 import './Register.css'
 import {useFormik} from 'formik'
 
-const validate = values => {
+const validate = (values) => {
     const errors = {};
     if (!values.name) {
       errors.name = 'Required';
